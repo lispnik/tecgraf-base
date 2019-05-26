a59c10fca574d8f4187e52867c99e8e11e7f89cd
Matthew Kennedy <burnsidemk@gmail.com>
2019-05-26 14:58:58 -0500

    Include im-file, im-image

--- tecgraf-base.lisp
+++ tecgraf-base.lisp
@@ -1,7 +1,11 @@
 (defpackage #:tecgraf-base
   (:use #:common-lisp)
-  (:export #:ihandle))
+  (:export #:ihandle
+           #:im-image
+           #:im-file))
 
 (in-package #:tecgraf-base)
 
 (pffft:define-foreign-pointer-wrapper ihandle)
+(pffft:define-foreign-pointer-wrapper im-image)
+(pffft:define-foreign-pointer-wrapper im-file)
