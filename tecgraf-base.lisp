151b080e1878d2330f5df15fbe86f030d6b2432e
Matthew Kennedy <burnsidemk@gmail.com>
2019-11-30 15:24:03 -0600

    Add a simple defalias definition

--- tecgraf-base.lisp
+++ tecgraf-base.lisp
@@ -2,10 +2,17 @@
   (:use #:common-lisp)
   (:export #:ihandle
            #:im-image
-           #:im-file))
+           #:im-file
+           #:defalias))
 
 (in-package #:tecgraf-base)
 
 (pffft:define-foreign-pointer-wrapper ihandle)
 (pffft:define-foreign-pointer-wrapper im-image)
 (pffft:define-foreign-pointer-wrapper im-file)
+
+(defmacro defalias (to what &optional documentation (documentation-type 'function))
+  `(progn
+     (setf (fdefinition ',to) ,what)
+     (when ,documentation
+       (setf (documentation ',to ',documentation-type) ,documentation))))
